0fac166dc0d45f62522833adb9d8c59df21037aa
Pranav Acharya <pranavac@buffalo.edu>
2026-03-13 14:34:22 -0400

    added ALU.v, CU.v

---
+++ sources_1/new/ALU.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Pranav Acharya
+// 
+// Create Date: 03/05/2026 04:12:22 PM
+// Design Name: 
+// Module Name: ALU
+// Project Name: CSE 490 Project 1
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU(
+    input wire [15:0] rs,
+    input wire [15:0] rdimm,
+    input wire [2:0] ALUop,
+    output wire [0:0] zero,
+    output reg [15:0] ALU_out
+    );
+    //variable names for ALUop signals
+    localparam ADD = 3'b00x;
+    localparam SUB = 3'b1xx;
+    localparam SLL = 3'b010;
+    localparam AND = 3'b011;
+    //main execution loop, constant
+    always @(*) begin
+       case (ALUop)
+        ADD: ALU_out = rs + rdimm;
+        SUB: ALU_out = rs - rdimm;
+        SLL: ALU_out = rs << rdimm[3:0];
+        AND: ALU_out = rs & rdimm;
+        default: ALU_out = 16'd0;
+       endcase
+             
+    end
+    
+    assign zero = (ALU_out == 16'd0);
+endmodule
---
+++ sources_1/new/CU.v
@@ -0,0 +1,119 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/09/2026 02:18:21 PM
+// Design Name: 
+// Module Name: CU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CU(
+    input wire [3:0] opcode,
+    input wire [3:0] func,
+    output reg [2:0] ALUop,
+    output reg ALUsrc, //0 is for register, 1 is for imm
+    output reg Branch, //0 is dont take, 1 is take branch
+    output reg ImmSel, //0 is 0 extend, 1 is sign extend
+    output reg MemWrite,  
+    output reg MemtoReg, 
+    output reg RegWrite
+    );
+    //opcodes
+    localparam ALU_R = 4'b0000;
+    localparam LW  = 4'b0001;
+    localparam SW  = 4'b0010;
+    localparam ADDI = 4'b0011;
+    localparam BEQ = 4'b0100;
+    localparam BNE = 4'b0101;
+    localparam JMP = 4'b0110;
+    //func codes
+    localparam ADD = 4'b0000;
+    localparam SUB = 4'b0001;
+    localparam SLL = 4'b0010;
+    localparam AND = 4'b0011;
+    
+    //main execution loop, constant
+    always @(*) begin
+        //defaults
+        ALUop    = 3'b000;
+        RegWrite = 0;
+        ALUsrc   = 0;
+        ImmSel   = 0;
+        Branch   = 0;
+        MemWrite = 0;
+        MemtoReg = 0;
+        case (opcode)
+         ALU_R:
+            case (func)
+                ADD: begin
+                    ALUop = 3'b000;
+                    RegWrite = 1;
+                    ALUsrc = 0;
+                    ImmSel = 0;
+                    end
+                SUB: begin
+                    ALUop = 3'b100;
+                    RegWrite = 1;
+                    ALUsrc = 0;
+                    ImmSel = 0;
+                    end
+                SLL: begin
+                    ALUop = 3'b010;
+                    RegWrite = 1;
+                    ALUsrc = 0;
+                    ImmSel = 0;
+                    end
+                AND: begin
+                    ALUop = 3'b011;
+                    RegWrite = 1;
+                    ALUsrc = 0;
+                    ImmSel = 0;
+                    end
+                BEQ: begin
+                    ALUop = 3'b100;
+                    RegWrite = 0;
+                    ALUsrc = 0;
+                    ImmSel = 0;
+                    Branch = 1;
+                    end
+            endcase
+         LW: begin
+                ALUop = 3'b000;
+                ALUsrc = 1;
+                ImmSel = 1;
+                MemtoReg = 1;
+                RegWrite = 1;
+            end
+         SW: begin
+                ALUop = 3'b000;
+                ALUsrc = 1;
+                ImmSel = 1;
+                MemWrite = 1;
+             end
+         ADDI: begin
+                ALUop = 3'b000;
+                ALUsrc = 1;
+                ImmSel = 1;
+                RegWrite = 1;
+                end
+         BEQ: begin
+                ALUop = 3'b100;
+                Branch = 1;
+                end
+        endcase
+        
+    end 
+    endmodule
