d36a4d1119b28077b8e4a665dcd9effddf22c630
nicolejulia817 <121770498+nicolejulia817@users.noreply.github.com>
2026-03-14 10:31:22 -0400

    Create IM.v

---
+++ sources_1/new/IM.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Nicole Oszczypala
+// 
+// Create Date: 03/05/2026 04:12:22 PM
+// Design Name:16-bit processor
+// Module Name: IM
+// Project Name: CSE 490 Project 1 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module IM(
+    
+    );
+    
+endmodule
