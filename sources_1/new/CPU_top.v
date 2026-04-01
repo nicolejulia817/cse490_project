895988d322e674e1f6167c129609067280111551
Pranav Acharya <pranavac@buffalo.edu>
2026-04-01 14:48:14 -0400

    added data memory and created empty top file

---
+++ sources_1/new/CPU_top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/01/2026 02:42:43 PM
+// Design Name: 
+// Module Name: CPU_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CPU_top(
+
+    );
+endmodule
