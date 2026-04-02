2df1dc81a8b0afcc310d7d7f4fbbb2186d020d46
nicolejulia817 <121770498+nicolejulia817@users.noreply.github.com>
2026-04-02 18:58:21 -0400

    Create Reg.v
    WIP

---
+++ sources_1/new/Reg.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/02/2026 06:26:00 PM
+// Design Name: 
+// Module Name: DataMem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RegisterFile #(
+    parameter NUM_REGS  = 16,   //$s0-$s15
+    parameter DATA_WIDTH = 16   // bits wide
+)(
+    input  wire        clk,
+
+    // rs
+    input  wire [3:0]  read_reg_1,
+    output wire [15:0] read_data_1,
+
+    // rt/rd 
+    input  wire [3:0]  read_reg_2,
+    output wire [15:0] read_data_2,
+
+    // Write (result from ALU or data memory (lw))
+    input  wire [3:0]  write_reg,
+    input  wire [15:0] write_data,
+    input  wire        reg_write
+);
+
+    // 16 registers, each 16 bits wide
+    reg [15:0] registers [0:NUM_REGS-1];
+
+
+endmodule
